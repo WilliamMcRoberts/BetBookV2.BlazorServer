4539115b0b870ed262453db00ae1ceb311102f09
WilliamMcRoberts <wamcroberts2@gmail.com>
2022-10-28 20:23:01 -0400

    svg draw on scroll animation, football spinners drop filter

--- BetBookGamingUserInterface/wwwroot/JS/Interop.js
+++ BetBookGamingUserInterface/wwwroot/JS/Interop.js
@@ -1,11 +1,8 @@
 ﻿
-
-window.addEventListener('scroll', () => {
-    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-}, false);
-
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollPos = window.pageYOffset;
+
+
 
 window.onscroll = () => {
     var currentScrollPos = window.pageYOffset;
@@ -18,3 +15,50 @@ window.onscroll = () => {
     prevScrollPos = currentScrollPos;
 }
 
+//window.addEventListener('scroll', setProp, false);
+
+//function setProp() {
+//    document.body.style.setProperty(
+//        '--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+//    );
+//}
+
+window.addEventListener('scroll', drawleft, false);
+
+function drawleft() {
+
+    var footballLeft = document.getElementById("path-left");
+    var length = footballLeft.getTotalLength();
+
+    footballLeft.style.stroke = '#fff';
+
+    footballLeft.style.strokeDasharray = length;
+
+    footballLeft.style.strokeDashoffset = length;
+
+    var scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+
+    var draw = length * scrollpercent;
+
+    footballLeft.style.strokeDashoffset = length - draw;
+}
+
+window.addEventListener('scroll', drawright, false);
+
+function drawright() {
+
+    var footballRight = document.getElementById("path-right");
+    var length = footballRight.getTotalLength();
+
+    footballRight.style.stroke = '#fff';
+
+    footballRight.style.strokeDasharray = length;
+
+    footballRight.style.strokeDashoffset = length;
+
+    var scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+
+    var draw = length * scrollpercent;
+
+    footballRight.style.strokeDashoffset = length - draw;
+}
